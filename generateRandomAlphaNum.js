4f739d60e98a12a7ebdfa8c9288423c9b50fa1a9
w3cmark <lijungm305@gmail.com>
2014-06-03 21:51:44 +0800

    Create generateRandomAlphaNum.js

---
+++ generateRandomAlphaNum.js
@@ -0,0 +1,6 @@
+//生成随机字符串
+function generateRandomAlphaNum(len) {
+    var rdmString = "";
+    for (; rdmString.length < len; rdmString += Math.random().toString(36).substr(2));
+    return rdmString.substr(0, len);
+}
